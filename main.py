4f10359642711a4479fe750eab9e0480fb51cec8
Ry Ferguson <rytf12@gmail.com>
2023-11-30 23:54:25 +0000

    add CORS middleware so that FastAPI and React can talk to each other when on different ports

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
@@ -50,6 +51,18 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
+
+origins = {
+    "http://localhost:3000",
+}
+
+app.add_middleware(
+   CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials =True,
+    allow_methods = ["*"],
+    allow_headers= ["*"],
+)
 
 
 def verify_password(plain_password, hashed_password):
